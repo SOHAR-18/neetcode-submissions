23d4846bd9a182ebb97dc08fc16e5c5ad6907e08
SOHAR-18 <soharbaba182005@gmail.com>
2026-04-30 00:43:17 +0200

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+       int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int freshCount =0;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]== 2){
+                    queue.offer(new int[]{i,j});
+                }else if(grid[i][j] == 1){
+                    freshCount++;
+                }
+            }
+        }
+        if(freshCount ==0) return 0;
+        int minute =0;
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            boolean rotted = false;
+            for(int i=0;i<size;i++){
+                int[] curr = queue.poll();
+                int row= curr[0];
+                int col = curr[1];
+
+                for(int[] dir : directions){
+                    int newRow = row+dir[0];
+                    int newCol = col + dir[1];
+                    if(newRow >=0 && newRow <rows && newCol >=0 && newCol <cols && grid[newRow][newCol] ==1){
+                        grid[newRow][newCol] =2;
+                        queue.offer(new int[]{newRow,newCol});
+                        freshCount--;
+                        rotted = true;
+                    }
+                }
+            }
+            if(rotted) minute++;
+        }
+        return freshCount ==0?minute:-1; 
+    }
+}
